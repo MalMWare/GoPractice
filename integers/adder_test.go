da75cfe972065a27b1d2b2bb84f3c8bcf33da732
Mal Martin <malkelleymartin@gmail.com>
2024-02-28 12:31:37 -0800

    must add output to get response

--- integers/adder_test.go
+++ integers/adder_test.go
@@ -14,7 +14,26 @@ func TestAdder(t *testing.T) {
 	}
 }
 
+// func ExampleAdd() {
+// 	sum := Add(1, 5)
+// 	fmt.Println(sum)
+// 	// output: 6
+// }
+
+// func ExampleAdd() {
+// 	sum := Add(1, 5)
+// 	fmt.Println(sum)
+// 	// Output: 6
+// }
+
+// func ExampleAdd() {
+// 	sum := Add(1, 5)
+// 	fmt.Println(sum)
+// 	//output:6
+// }
+
 func ExampleAdd() {
 	sum := Add(1, 5)
 	fmt.Println(sum)
+	// Output: 6
 }
