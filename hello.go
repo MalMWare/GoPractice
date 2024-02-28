52c7c88da444df1b84a0f479e3e2106c4f9b80c7
Mal Martin <Mal@viticusgroup.org>
2024-02-27 16:10:40 -0800

    passed for testing

--- hello.go
+++ hello.go
@@ -2,6 +2,17 @@ package main
 
 import "fmt"
 
+// Basics
+// func main() {
+// 	fmt.Println("Hello, world")
+// }
+
+//How to test the code
+
+func Hello() string {
+	return "Hello, world"
+}
+
 func main() {
-	fmt.Println("Hello, world")
+	fmt.Println(Hello())
 }
