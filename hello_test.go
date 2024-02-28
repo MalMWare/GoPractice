8d0107179235f2f017a4642f8c70a304c9276c6c
Mal Martin <malkelleymartin@gmail.com>
2024-02-28 10:44:44 -0800

    added more parameters

--- hello_test.go
+++ hello_test.go
@@ -3,16 +3,28 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Mal")
+	t.Run("to a person", func(t *testing.T) {
+		got := Hello("Mal", "")
 		want := "Hello, Mal"
 		assertCorrectMessage(t, got, want)
 	})
-	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+
+	t.Run("empty string", func(t *testing.T) {
+		got := Hello("", "")
 		want := "Hello, World"
 		assertCorrectMessage(t, got, want)
+	})
 
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Juan", spanish)
+		want := "Hola, Juan"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Jean", french)
+		want := "Bonjour, Jean"
+		assertCorrectMessage(t, got, want)
 	})
 }
 
