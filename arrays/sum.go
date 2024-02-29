8c515f79edd4507750edae6d1d5634c5531680b9
Mal Martin <malkelleymartin@gmail.com>
2024-02-29 12:07:31 -0800

    refactor with range

--- arrays/sum.go
+++ arrays/sum.go
@@ -1,9 +1,21 @@
 package main
 
+//below is a way to pass the test
+
+// func Sum(numbers [5]int) int {
+// 	sum := 0
+// 	for i := 0; i < 5; i++ {
+// 		sum += numbers[i]
+// 	}
+// 	return sum
+// }
+
+//However, range lets you iterate over an array. On each iteration, range returns two values - the index and the value. We are choosing to ignore the index value by using _
+// i = number
 func Sum(numbers [5]int) int {
 	sum := 0
-	for i := 0; i < 5; i++ {
-		sum += numbers[i]
+	for _, i := range numbers {
+		sum += i
 	}
 	return sum
 }
